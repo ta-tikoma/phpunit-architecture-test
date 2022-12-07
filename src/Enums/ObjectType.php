41a51f5731c0e900eab10ba72cc34d26aec5788e
Nuno Maduro <enunomaduro@gmail.com>
2022-12-07 15:46:16 +0000

    Adds support for enums

--- src/Enums/ObjectType.php
+++ src/Enums/ObjectType.php
@@ -8,12 +8,14 @@ use MyCLabs\Enum\Enum;
 
 /**
  * @method static static _CLASS()
+ * @method static static _ENUM()
  * @method static static _TRAIT()
  * @method static static _INTERFACE()
  */
 final class ObjectType extends Enum
 {
     private const _CLASS     = 'class';
+    private const _ENUM      = 'enum';
     private const _TRAIT     = 'trait';
     private const _INTERFACE = 'interface';
 }
